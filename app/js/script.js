a07f307df6f04dc8025c613382f52ea57e2e33a1
ryanbradley23 <hoopingstars23@gmail.com>
2024-10-16 09:55:05 +0300

    Front-end

--- app/js/script.js
+++ app/js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'UI/UX Designer'],
+    strings: ['Front-end Developer', 'UI/UX Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
